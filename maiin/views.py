2f759d025a5ab560089f2ee58565f008d4832484
mhrfqi <2311102441017@umkt.ac.id>
2025-03-08 16:46:22 +0800

    Mau masukan folder dan file buat tugas pratikum 3

---
+++ maiin/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+
+def home(request):
+    template_name = 'portfolio/home.html'
+    context = {
+        'title': 'Muhamamd Rifqi Dzakwan',
+        'description': 'web portfolio saya',
+        'body': 'Halaman Utama'
+    }
+    return render(request, template_name, context)
